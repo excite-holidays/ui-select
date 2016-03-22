628a8c95dfeb312b133c4ffc29096e8f3f039dc1
Pratchaya Bangthong <pratchaya.bangthong@hotmail.com>
2016-03-22 10:30:57 +0700

    Update condition to update selected index

--- src/uiSelectController.js
+++ src/uiSelectController.js
@@ -45,7 +45,7 @@ uis.controller('uiSelectCtrl',
   if (ctrl.searchInput.length !== 1) {
     throw uiSelectMinErr('searchInput', "Expected 1 input.ui-select-search but got '{0}'.", ctrl.searchInput.length);
   }
-  
+
   ctrl.isEmpty = function() {
     return angular.isUndefined(ctrl.selected) || ctrl.selected === null || ctrl.selected === '';
   };
@@ -55,7 +55,7 @@ uis.controller('uiSelectCtrl',
     if (ctrl.resetSearchInput || (ctrl.resetSearchInput === undefined && uiSelectConfig.resetSearchInput)) {
       ctrl.search = EMPTY_SEARCH;
       //reset activeIndex
-      if (ctrl.selected && ctrl.items.length && !ctrl.multiple) {
+      if (ctrl.selected !== undefined && ctrl.selected !== null && ctrl.items.length && !ctrl.multiple) {
         ctrl.activeIndex = ctrl.items.indexOf(ctrl.selected);
       }
     }
@@ -149,7 +149,7 @@ uis.controller('uiSelectCtrl',
     //If collection is an Object, convert it to Array
 
     var originalSource = ctrl.parserResult.source;
-    
+
     //When an object is used as source, we better create an array and use it as 'source'
     var createArrayFromObject = function(){
       var origSrc = originalSource($scope);
@@ -195,7 +195,7 @@ uis.controller('uiSelectCtrl',
         ctrl.items = [];
       } else {
         if (!angular.isArray(items)) {
-          throw uiSelectMinErr('items', "Expected an array but got '{0}'.", items);          
+          throw uiSelectMinErr('items', "Expected an array but got '{0}'.", items);
         } else {
           //Remove already selected items (ex: while searching)
           //TODO Should add a test
